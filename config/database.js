df89ee8d9059725468d544c5f32dd2402b5c60dc
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-02-01 20:43:46 +0600

    configured

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
-const path = require('path');
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    options: {
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+    debug: false,
   },
 });
